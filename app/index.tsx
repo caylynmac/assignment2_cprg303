8bb38c5a8eca9bec8f9d56c23a07f66f2258cfaf
Nicholas Graham <nsgraham98@gmail.com>
2025-02-11 09:45:52 -0700

    better App.tsx fix
    following feb 4th class example

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useState } from 'react';
-import SignIn from '../components/SignIn';
+import SignIn from '../components/signin';
 
 export default function App() {
     //global state variables
